a6698e8b1168feb29ea954b1727bd3072cecf5e8
gonasoo <happysun7080@naver.com>
2021-09-08 20:14:43 +0900

    210908 #위클리_챌린지 '1주차_부족한_금액_계산하기'

---
+++ Programmers/위클리_챌린지-1주차_부족한_금액_계산하기.py
@@ -0,0 +1,11 @@
+# 1차: 210908
+
+'''
+위클리 챌린지 - 1주차_부족한 금액 계산하기
+'''
+
+def solution(price, money, count):
+    answer = ((count*(count+1))//2)*price - money
+    return answer if answer > 0 else 0
+
+print(solution(3, 20, 4))
