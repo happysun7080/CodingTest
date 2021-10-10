56bc3bf25be9ed2b0cb88bb557132e61966be819
gonasoo <happysun7080@naver.com>
2021-10-10 22:53:36 +0900

    211010 #1436 '영화감독 숌' (BruteForce)

---
+++ BOJ/1436.py
@@ -0,0 +1,19 @@
+### Silver 5 ###
+
+# 1차: 210122
+# 2차: 211010
+
+'''
+영화감독 숌
+- BruteForce
+'''
+
+
+N = int(input())
+r = 665
+ct = 0
+while ct != N:
+    r += 1
+    if '666' in str(r):
+        ct += 1
+print(r)
