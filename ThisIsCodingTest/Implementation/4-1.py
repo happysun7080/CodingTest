4dc8c067dbf750dd8ca2ed716c12b34dc32b6d1e
gonasoo <happysun7080@naver.com>
2022-03-07 18:40:52 +0900

    220307 level: 1 상하좌우

---
+++ ThisIsCodingTest/Implementation/4-1.py
@@ -0,0 +1,28 @@
+### Implementation ###
+
+'''
+4-1 상하좌우
+
+level: 1/3
+'''
+
+N = int(input())
+Road = list(input().split())
+
+x = y = 1
+
+for r in Road:
+    if r == 'L':
+        if y != 1:
+            y -= 1
+    elif r == 'R':
+        if y != N:
+            y += 1
+    elif r == 'U':
+        if x != 1:
+            x -= 1
+    elif r == 'D':
+        if x != N:
+            x += 1
+
+print(x, y)
