17f8a17fb485f3289c1f011f7f88d5f25c0d0bce
gonasoo <happysun7080@naver.com>
2022-03-09 22:43:15 +0900

    220309 level: 1 - 숫자 카드 게임

---
+++ ThisIsCodingTest/Greedy/3-4.py
@@ -0,0 +1,24 @@
+### Greedy ###
+
+'''
+3-4 숫자 카드 게임
+
+level: 1/3
+reference: 2019 국가 교육기관 코딩 테스트
+'''
+
+N, M = map(int, input().split())
+
+rlt = 0
+
+for i in range(N):
+    card = list(map(int, input().split()))
+    m = 10001
+
+    for j in card:
+        if j < m:
+            m = j
+
+    rlt = max(rlt, m)
+
+print(rlt)
