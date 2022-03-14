5db3c6fea85ce280c91906c070e817fee6f1b92c
gonasoo <happysun7080@naver.com>
2022-03-14 22:57:06 +0900

    220314 level: 1 - DFS 예제

---
+++ ThisIsCodingTest/DFS,BFS/5-8.py
@@ -0,0 +1,33 @@
+### DFS/BFS ###
+
+'''
+5-8 DFS 예제
+
+level: 1/3
+'''
+
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+dfs(graph, 1, visited)
