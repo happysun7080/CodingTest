8a65aa84ec8f29027b37ebe8dd03a2c4f3e8221d
gonasoo <happysun7080@naver.com>
2022-03-18 23:30:48 +0900

    220318 level: 1 - swap

---
+++ ThisIsCodingTest/Sorting/6-2.py
@@ -0,0 +1,20 @@
+### Sorting ###
+
+'''
+6-2 swap
+
+level: 1/3
+'''
+
+array = [3, 5]
+array[0], array[1] = array[1], array[0]
+
+print(array)
+
+# C style
+a = 3
+b = 5
+
+temp = a
+a = b
+b = temp
