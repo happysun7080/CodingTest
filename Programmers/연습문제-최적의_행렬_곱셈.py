ab35fa1658725924f1d685ad6f2642cb52839467
GwansuShin <happysun7080@gmail.com>
2022-07-29 16:24:44 +0900

    220729 #연습문제 - 최적의 행렬 곱셈

--- Programmers/연습문제-최적의_행렬_곱셈.py
+++ Programmers/연습문제-최적의_행렬_곱셈.py
@@ -1,8 +1,30 @@
 ### level 3 ###
 
 # 1차: 220719
+# 2차: 220729
 
 '''
 연습문제 - 최적의 행렬 곱셈 (12942)
 '''
 
+
+def solution(matrix_sizes):
+    dp = [[0 for _ in range(len(matrix_sizes))]
+          for _ in range(len(matrix_sizes))]
+
+    for i in range(1, len(matrix_sizes)):
+        for j in range(0, len(matrix_sizes) - i):
+            k = j + i
+
+            cur = []
+            for m in range(j, k):
+                cur.append(
+                    dp[j][m] +
+                    dp[m+1][k] +
+                    matrix_sizes[j][0] * matrix_sizes[m][1] * matrix_sizes[k][1])
+            dp[j][k] = min(cur)
+
+    return dp[0][-1]
+
+
+# print(solution([[5, 3], [3, 10], [10, 6]]))
