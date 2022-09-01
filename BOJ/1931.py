53290a129e72525abff0b70c7b4bd5822b9e4e71
GwansuShin <happysun7080@naver.com>
2022-09-01 17:20:20 +0900

    220901 #1931 '회의실 배정' (그리디 알고리즘, 정렬)

--- BOJ/1931.py
+++ BOJ/1931.py
@@ -10,21 +10,21 @@
 
 
 import sys
-from collections import defaultdict
 
 input = sys.stdin.readline
-INF = 2**31 - 1
 
 N = int(input())
 meet = []
 for _ in range(N):
     meet.append(list(map(int, input().split())))
 
-meet.sort(key=lambda x: (x[0], x[1]))
+# meet.sort(key=lambda x: (x[0], x[1]))
+meet = sorted(meet, key=lambda x: (x[0]))
+meet = sorted(meet, key=lambda x: (x[1]))
 
 answer, end = 0, 0
 for s, e in meet:
-    if s > end:
+    if s >= end:
         end = e
         answer += 1
 
