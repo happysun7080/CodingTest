60db8d71f30f4d033a727c59974f871ae39ad152
GwansuShin <happysun7080@naver.com>
2022-09-05 16:43:19 +0900

    220905 #11052 '카드 구매하기' (DP)

---
+++ BOJ/11052.py
@@ -0,0 +1,23 @@
+### Silver 1 ###
+
+# 1차: 220905
+
+'''
+카드 구매하기
+- DP
+'''
+
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+P = [0] + list(map(int, input().split()))
+dp = [0 for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i], dp[i-j] + P[j])
+
+print(dp[-1])
